d3771bbde6426b4f120d65e0426793150fb50ada
AlvaroBuleo <algobul@gmail.com>
2023-10-03 19:46:45 +0200

    Añadida implementación de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -14,4 +14,12 @@ class ListArray : public List<T> {
 		T operator[](int pos);
 		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list);
 		void resize(int new_size);
+		virtual void insert(int pos,T e) override;
+		virtual void append(T e) override;
+		virtual void prepend(T e) override;
+		virtual T remove(int pos) override;
+		virtual T get(int pos) override;
+		virtual int search(T e) override;
+		virtual bool empty() override;
+		virtual int size() override;
 };
